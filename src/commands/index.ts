75849c64983755c86b66cf46d8e2ecc7f469b43f
segfault0x61 <torresj1997@outlook.com>
2022-04-26 22:03:46 -0500

    feat: add whatanime command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ import github from './github';
 import help from './help';
 import listguild from './listGuild';
 import userinfo from './userinfo';
+import whatAnime from './whatAnime';
 
-export { github, help, listguild, userinfo };
+export { github, help, listguild, userinfo, whatAnime };
